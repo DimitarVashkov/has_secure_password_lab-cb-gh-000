ae33309b136adad1122f7cd6ac1e1384b34b5434
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-16 02:28:33 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params).save
-  end
+  @user = User.create(user_params)
+  return redirect_to controller: 'users', action: 'new' unless @user.save
+  session[:user_id] = @user.id
+  redirect_to controller: 'welcome', action: 'home'
+end
 
+private
 
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
+def user_params
+  params.require(:user).permit(:name, :password, :password_confirmation)
+end
 end
